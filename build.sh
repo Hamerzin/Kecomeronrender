658c380bb2fc6dc66ba0ad80cdc51e3cdacdb86a
hamerzin <dipaolo@gmail.com>
2022-10-06 17:02:46 -0300

    primer commit

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --noinput --email "dipaolo@gmail.com" --password "dipa1983"
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
