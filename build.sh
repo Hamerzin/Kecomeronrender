c7cd4c49a5ffa0e34a13a3058717f36e88aa5390
hamerzin <dipaolo@gmail.com>
2022-10-06 16:54:54 -0300

    primer commit

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-
+python manage.py makemigrations
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py createsuperuser --noinput --email "dipaolo@gmail.com" --password "dipa1983"
