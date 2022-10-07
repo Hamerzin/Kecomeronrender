f66cd5c25e07f9c9d73c4de3d0443e1beae653ce
hamerzin <dipaolo@gmail.com>
2022-10-07 19:12:59 -0300

    feat: actualizaciones

--- build.sh
+++ build.sh
@@ -3,10 +3,9 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py makemigrations
 python manage.py collectstatic --no-input
 python manage.py migrate
 #esto crea un super usuario la primera vez en render
-#export DJANGO_SUPERUSER_EMAIL=dipa@dipa.com
-#export DJANGO_SUPERUSER_PASSWORD=dipa1983
-#python manage.py createsuperuser --no-input 
+export DJANGO_SUPERUSER_EMAIL=dipa@dipa.com
+export DJANGO_SUPERUSER_PASSWORD=dipa1983
+python manage.py createsuperuser --no-input 
