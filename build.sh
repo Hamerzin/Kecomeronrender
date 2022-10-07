f756451309ee7690291a37d1cbd7ba5fa5b37edf
hamerzin <dipaolo@gmail.com>
2022-10-07 13:51:57 -0300

    feat: actualizaciones

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py makemigrations
+python manage.py makemigrations --merge
 python manage.py collectstatic --no-input
 python manage.py migrate
 #esto crea un super usuario la primera vez en render
