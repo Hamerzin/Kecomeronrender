887d2ba6050758c832270eee21f8c1f95a163062
hamerzin <dipaolo@gmail.com>
2022-10-17 21:21:10 -0300

    cambios

--- recipes/genericviews.py
+++ recipes/genericviews.py
@@ -7,14 +7,8 @@ from drf_yasg.utils import swagger_auto_schema
 
 
 class RecipesViewset(ModelViewSet):
-
-    serializer_class=RecipesSerializer
-    queryset=RecipesModel.objects.all()
-    
-
     serializer_class = RecipesSerializer
     queryset = RecipesModel.objects.all()
-
 
     @swagger_auto_schema(operation_summary="List Product", operation_description="This endpoint list a users")
     def list(self, request, *args, **kwargs):
@@ -23,30 +17,9 @@ class RecipesViewset(ModelViewSet):
         filter_queryset = self.filter_jefatura(serializer)
         return Response(filter_queryset)
 
-
-
-class RecipesSearch(generics.ListAPIView):
-    queryset = RecipesModel.objects.all()
-    serializer_class = RecipesSerializer
-
-    
-
-    @swagger_auto_schema(operation_summary="List user", operation_description="This endpoint list a users")
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-
     def filter_jefatura(self, serializer):
         new_query = defaultdict(list)
         for row in serializer.data:
             category = row.get('category')
             new_query[category].append(row)
         return new_query
-
